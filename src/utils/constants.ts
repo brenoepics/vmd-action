b3bb6dead430d289d7a6171520468e3bd2f69e0a
Breno A. <git@breno.tech>
2024-09-26 03:41:44 -0300

    fix: remove color tags only

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,15 +1,17 @@
 export const LOW_HEALTH_THRESHOLD: number = 75;
 export const MEDIUM_HEALTH_THRESHOLD: number = 85;
 export const OK_HEALTH_THRESHOLD: number = 95;
-export const tagsRemover: (str: string) => string = (str: string) => str.replace(/<bg_err>/g, "")
-  .replace(/<bg_warn>/g, "")
-  .replace(/<bg_info>/g, "")
-  .replace(/<bg_ok>/g, "")
-  .replace(/<\/bg_err>/g, "")
-  .replace(/<\/bg_warn>/g, "")
-  .replace(/<\/bg_info>/g, "")
-  .replace(/<\/bg_ok>/g, "")
-  .replace(/<text_warn>/g, "")
-  .replace(/<text_info>/g, "")
-  .replace(/<\/text_warn>/g, "")
-  .replace(/<\/text_info>/g, "");
+export const tagsRemover: (str: string) => string = (str: string) =>
+  str
+    .replace(/<bg_err>/g, "")
+    .replace(/<bg_warn>/g, "")
+    .replace(/<bg_info>/g, "")
+    .replace(/<bg_ok>/g, "")
+    .replace(/<\/bg_err>/g, "")
+    .replace(/<\/bg_warn>/g, "")
+    .replace(/<\/bg_info>/g, "")
+    .replace(/<\/bg_ok>/g, "")
+    .replace(/<text_warn>/g, "")
+    .replace(/<text_info>/g, "")
+    .replace(/<\/text_warn>/g, "")
+    .replace(/<\/text_info>/g, "");
