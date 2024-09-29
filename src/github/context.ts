3ba2bebe86a7a36a656d4889bf308dde8564ac42
Breno A. <git@breno.tech>
2024-09-28 23:52:16 -0300

    feat: branch comparison

--- src/github/context.ts
+++ src/github/context.ts
@@ -3,6 +3,9 @@ import { WebhookPayload } from "@actions/github/lib/interfaces.js";
 
 export const PAYLOAD: WebhookPayload = github.context.payload;
 export const IS_PULL_REQUEST: boolean = PAYLOAD.pull_request !== undefined;
-export const CURRENT_BRANCH: string = github.context.ref;
+export const CURRENT_BRANCH: string = github.context.ref.replace(
+  "refs/heads/",
+  ""
+);
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
 export const TARGET_BRANCH: string | undefined = PAYLOAD.pull_request?.base.ref;
