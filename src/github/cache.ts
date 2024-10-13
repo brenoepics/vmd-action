1bfaa1637b1238ef1098fba692f13a5374cbf530
Breno A. <git@breno.tech>
2024-10-13 00:38:27 -0300

    fix: cache file missmatch

--- src/github/cache.ts
+++ src/github/cache.ts
@@ -1,13 +1,16 @@
 import * as core from "@actions/core";
 import * as cache from "@actions/cache";
+import * as github from "@actions/github";
 import { VMDAnalysis } from "../types.js";
 import { parseAnalysisOutput } from "../helpers/parser.js";
+
+const workflow: string = github.context.workflow;
 
 export async function saveCache(
   filePath: string,
   branch: string
 ): Promise<void> {
-  const cacheId: string = `vmd-analysis-${branch}`;
+  const cacheId: string = `vmd-analysis-${branch}-${workflow}`;
   const cachePaths: string[] = [filePath];
   try {
     await cache.saveCache(cachePaths, cacheId);
@@ -20,7 +23,7 @@ export async function restoreCache(
   branch: string,
   cachePath: string
 ): Promise<VMDAnalysis | undefined> {
-  const cacheId: string = `vmd-analysis-${branch}`;
+  const cacheId: string = `vmd-analysis-${branch}-${workflow}`;
   try {
     const cacheKey: string | undefined = await cache.restoreCache(
       [cachePath],
